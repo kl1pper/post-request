aaab7c85273bea9be0f762c8eed0f6572a9623c3
dmitry <kl1pper3@gmail.com>
2015-10-31 16:49:42 +0500

    init commit

---
+++ src/main/java/ru/dmitry/domain/City.java
@@ -0,0 +1,35 @@
+package ru.dmitry.domain;
+
+
+public class City {
+
+    private Integer id;
+
+    private String title;
+
+    private Country country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
---
+++ src/main/java/ru/dmitry/domain/Country.java
@@ -0,0 +1,47 @@
+package ru.dmitry.domain;
+
+
+import java.util.List;
+
+public class Country {
+
+    private Integer id;
+
+    private String title;
+
+    private String ISOCode;
+
+    private List<City> cities;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getISOCode() {
+        return ISOCode;
+    }
+
+    public void setISOCode(String ISOCode) {
+        this.ISOCode = ISOCode;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(List<City> cities) {
+        this.cities = cities;
+    }
+}
